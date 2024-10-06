777ef504636beb040ec59d145a9805c788a2b2df
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-10-06 11:04:32 +0530

    Receiving Form Data

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -12,14 +12,20 @@ export default function TopicCreateForm() {
   return (
     <Popover placement="left">
       <PopoverTrigger>
-        <Button color="primary">Create a Topic</Button>`1`
+        <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
         <form action={actions.createTopic}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
-            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+            />
             <Textarea
+              name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
